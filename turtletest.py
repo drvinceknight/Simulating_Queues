d4fdca66f6f68b44e751e45abac902a598262bbe
vince <vincent.knight@gmail.com>
2013-10-25 19:28:52 +0100

    bug with player ending service and starting service again if queue empty or something...

--- turtletest.py
+++ turtletest.py
@@ -3,11 +3,12 @@ from turtle import Turtle, mainloop
 from random import expovariate as randexp
 
 class Player(Turtle):
-    def __init__(self, lmbda, mu, queue):
+    def __init__(self, lmbda, mu, queue, server):
         Turtle.__init__(self)
         self.interarrivaltime = randexp(lmbda)
         self.servicetime = randexp(mu)
         self.queue = queue
+        self.server = server
         self.shape('circle')
     def move(self, x, y):
         self.setx(x)
@@ -21,13 +22,26 @@ class Player(Turtle):
         self.move(self.queue.position[0] + 5, self.queue.position[1])
         self.color('blue')
         self.queue.join(self)
+    def startservice(self, t):
+        self.move(self.server.position[0], self.server.position[1])
+        self.servicedate = t + self.servicetime
+        self.server.start(self)
+        self.color('green')
+    def endservice(self):
+        self.move(self.server.position[0] + 50, self.server.position[1] - 50)
+        self.color('black')
+        self.server.players = self.server.players[1:]
 
 class Queue():
     def __init__(self, qposition):
         self.players = []
-        self.position = list(qposition)
+        self.position = qposition
     def __iter__(self):
         return iter(self.players)
+    def __len__(self):
+        return len(self.players)
+    def pop(self, index):
+        return self.players.pop(index)
     def join(self, player):
         self.players.append(player)
         print self.position
@@ -38,10 +52,26 @@ class Queue():
             y = p.position()[1]
             p.move(x + 10, y)
 
+class Server():
+    def __init__(self, svrposition):
+        self.players = []
+        self.position = svrposition
+    def __iter__(self):
+        return iter(self.players)
+    def __len__(self):
+        return len(self.players)
+    def start(self,player):
+        self.players.append(player)
+        self.players = sorted(self.players, key = lambda x : x.servicedate)
+        self.nextservicedate =  self.players[0].servicedate
+    def free(self):
+        return len(self.players) == 0
+
 class Sim():
-    def __init__(self, N, lmbda, mu, qposition=(200,-200)):
+    def __init__(self, N, lmbda, mu, qposition=[200,-200]):
         self.queue = Queue(qposition)
-        self.players = [Player(lmbda, mu, self.queue) for k in range(N)]
+        self.server = Server([qposition[0] + 20, qposition[1]])
+        self.players = [Player(lmbda, mu, self.queue, self.server) for k in range(N)]
     def run(self):
         t = 0
         nextplayer = self.players.pop()
@@ -49,11 +79,19 @@ class Sim():
         nextplayer.joinqueue()
         while self.players:
             t += 1
+            if self.server.free():
+                if len(self.queue) == 0:
+                    nextplayer.startservice(t)
+                else:
+                    nextservice = self.queue.pop(0)
+                    nextservice.startservice(t)
+            elif t > self.server.nextservicedate:
+                self.server.players[0].endservice()
             if t > self.players[-1].interarrivaltime + nextplayer.arrivaldate:
                 nextplayer = self.players.pop()
                 nextplayer.arrive(t)
                 nextplayer.joinqueue()
 
 if __name__ == '__main__':
-    q = Sim(50, .01, 5)
+    q = Sim(50, .01, .02)
     q.run()
