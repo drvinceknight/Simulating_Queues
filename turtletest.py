0a1d467bc7b12fa511f47a67692b45afc3de4b40
vince <vincent.knight@gmail.com>
2013-10-25 21:26:28 +0100

    have basic mm1 queue simulated

--- turtletest.py
+++ turtletest.py
@@ -10,6 +10,7 @@ class Player(Turtle):
         self.queue = queue
         self.server = server
         self.shape('circle')
+        self.served = False
     def move(self, x, y):
         self.setx(x)
         self.sety(y)
@@ -23,14 +24,17 @@ class Player(Turtle):
         self.color('blue')
         self.queue.join(self)
     def startservice(self, t):
-        self.move(self.server.position[0], self.server.position[1])
-        self.servicedate = t + self.servicetime
-        self.server.start(self)
-        self.color('green')
+        if not self.served:
+            self.move(self.server.position[0], self.server.position[1])
+            self.servicedate = t + self.servicetime
+            self.server.start(self)
+            self.color('green')
     def endservice(self):
         self.move(self.server.position[0] + 50, self.server.position[1] - 50)
         self.color('black')
         self.server.players = self.server.players[1:]
+        self.served = True
+
 
 class Queue():
     def __init__(self, qposition):
@@ -41,16 +45,16 @@ class Queue():
     def __len__(self):
         return len(self.players)
     def pop(self, index):
+        for p in self.players[:index] + self.players[index + 1:]:  # Shift everyone up one queue spot
+            x = p.position()[0]
+            y = p.position()[1]
+            p.move(x + 10, y)
+        self.position[0] += 10  # Reset queue position for next arrivals
         return self.players.pop(index)
     def join(self, player):
         self.players.append(player)
         print self.position
         self.position[0] -= 10
-    def leave(self, player):
-        for p in self:
-            x = p.position()[0]
-            y = p.position()[1]
-            p.move(x + 10, y)
 
 class Server():
     def __init__(self, svrposition):
@@ -69,15 +73,17 @@ class Server():
 
 class Sim():
     def __init__(self, N, lmbda, mu, qposition=[200,-200]):
+        self.N = N
         self.queue = Queue(qposition)
-        self.server = Server([qposition[0] + 20, qposition[1]])
+        self.server = Server([qposition[0] + 50, qposition[1]])
         self.players = [Player(lmbda, mu, self.queue, self.server) for k in range(N)]
+        self.completed = []
     def run(self):
         t = 0
         nextplayer = self.players.pop()
         nextplayer.arrive(t)
         nextplayer.joinqueue()
-        while self.players:
+        while len(self.completed) < self.N:
             t += 1
             if self.server.free():
                 if len(self.queue) == 0:
@@ -86,12 +92,13 @@ class Sim():
                     nextservice = self.queue.pop(0)
                     nextservice.startservice(t)
             elif t > self.server.nextservicedate:
+                self.completed.append(self.server.players[0])
                 self.server.players[0].endservice()
-            if t > self.players[-1].interarrivaltime + nextplayer.arrivaldate:
+            if self.players and t > self.players[-1].interarrivaltime + nextplayer.arrivaldate:
                 nextplayer = self.players.pop()
                 nextplayer.arrive(t)
                 nextplayer.joinqueue()
 
 if __name__ == '__main__':
-    q = Sim(50, .01, .02)
+    q = Sim(50, .015, .02)
     q.run()
