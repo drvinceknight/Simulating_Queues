8809478d78ccea27c20daaa2a26d61293047d912
drvinceknight <vincent.knight@gmail.com>
2013-10-29 10:06:54 +0000

    documentation written for players

--- graphicalMM1.py
+++ graphicalMM1.py
@@ -1,36 +1,97 @@
 #!/usr/bin/env python
-from __future__ import division
-from turtle import Turtle, mainloop, setworldcoordinates
-from random import expovariate as randexp, random
-import sys
+"""
+Library with some objects that make use of the python Turtle library to show graphics of a discrete event simulation of an MM1 queue (random arrivals and services, a single server).
+
+There are various objects that allow for the simulation and demonstration of emergent behaviour:
+
+- Player (I use the term player instead of customer as I also allow for the selfish and optimal behaviour: graphical representation: blue coloured dot);
+- SelfishPlayer (inherited from Player: when passed a value of service, a SelfishPlayer will join the queue if and only if it is in their selfish interest: graphical representation: red coloured dot.);
+- OptimalPlayer (uses a result from Naor to ensure that the mean cost is reduced: graphical representation: gold coloured dot.)
+
+- Queue
+- Server
+
+- Sim (this is the main object that generates all other objects as required).
+"""
+from __future__ import division  # Simplify division
+from turtle import Turtle, mainloop, setworldcoordinates  # Commands needed from Turtle
+from random import expovariate as randexp, random  # Pseudo random number generation
+import sys  # Use to write to out
 
 def mean(lst):
+    """
+    Function to return the mean of a list.
+
+    Argument: lst - a list of numeric variables
+
+    Output: the mean of lst
+    """
     return sum(lst) / len(lst)
 
 def movingaverage(lst):
     """
     Custom built function to obtain moving average
+
+    Argument: lst - a list of numeric variables
+
+    Output: a list of moving averages
     """
     return [mean(lst[:k]) for k in range(1 , len(lst) + 1)]
 
 def naorthreshold(lmbda, mu, costofbalking):
     """
-    Function to return Naor's threshold for optimal behaviour in an M/M/1 queu
+    Function to return Naor's threshold for optimal behaviour in an M/M/1 queue. This is taken from Naor's 1969 paper: 'The regulation of queue size by Levying Tolls'
+
+    Arguments:
+        lmbda - arrival rate (float)
+        mu - service rate (float)
+        costofbalking - the value of service, converted to time units. (float)
+
+    Output: A threshold at which optimal customers must no longer join the queue (integer)
     """
-    n = 0
-    center = mu * costofbalking
+    n = 0  # Initialise n
+    center = mu * costofbalking  # Center mid point of inequality from Naor's aper
     rho = lmbda / mu
     while True:
         LHS = (n*(1-rho)- rho * (1-rho**n))/((1-rho)**2)
         RHS = ((n+1)*(1- rho)-rho*(1-rho**(n+1)))/((1-rho)**2)
         if LHS <= center and center <RHS:
             return n
-        n += 1
+        n += 1  # Continually increase n until LHS and RHS are either side of center
 
 
 class Player(Turtle):
+    """
+    A generic class for our 'customers'. I refer to them as players as I like to consider queues in a game theoretical framework. This class is inherited from the Turtle class so as to have the graphical interface.
+
+    Attributes:
+        lmbda: arrival rate (float)
+        mu: service rate (float)
+        queue: a queue object
+        server: a server object
+
+
+    Methods:
+        move - move player to a given location
+        arrive - a method to make our player arrive at the queue
+        startservice - a method to move our player from the queue to the server
+        endservice - a method to complete service
+    """
     def __init__(self, lmbda, mu, queue, server, speed):
-        Turtle.__init__(self)
+        """
+        Arguments:
+            lmbda: arrival rate (float)
+            interarrivaltime: a randomly sampled interarrival time (negative exponential for now)
+            mu: service rate (float)
+            service: a randomly sampled service time (negative exponential for now)
+            queue: a queue object
+            shape: the shape of our turtle in the graphics (a circle)
+            server: a server object
+            served: a boolean that indicates whether or not this player has been served.
+            speed: a speed (integer from 0 to 10) to modify the speed of the graphics
+            balked: a boolean indicating whether or not this player has balked (not actually needed for the base Player class... maybe remove... but might be nice to keep here...)
+        """
+        Turtle.__init__(self)  # Initialise all base Turtle attributes
         self.interarrivaltime = randexp(lmbda)
         self.lmbda = lmbda
         self.mu = mu
@@ -42,15 +103,38 @@ class Player(Turtle):
         self.speed(speed)
         self.balked = False
     def move(self, x, y):
+        """
+            A method that moves our player to a given point
+
+            Arguments:
+                x: the x position on the canvas to move the player to
+                y: the y position on the canvas to move the player to.
+
+            Output: NA
+        """
         self.setx(x)
         self.sety(y)
     def arrive(self, t):
+        """
+        A method that make our player arrive (the player is first created to generate an interarrival time, service time etc...).
+
+        Arguments: t the time of arrival (a float)
+
+        Output: NA
+        """
         self.penup()
         self.arrivaldate = t
         self.move(self.queue.position[0] + 5, self.queue.position[1])
         self.color('blue')
         self.queue.join(self)
     def startservice(self, t):
+        """
+        A method that makes our player start service (This moves the graphical representation of the player and also make the queue update it's graphics).
+
+        Arguments: t the time of service start (a float)
+
+        Output: NA
+        """
         if not self.served and not self.balked:
             self.move(self.server.position[0], self.server.position[1])
             self.servicedate = t + self.servicetime
@@ -58,6 +142,13 @@ class Player(Turtle):
             self.color('green')
             self.endqueuedate = t
     def endservice(self):
+        """
+        A method that makes our player end service (This moves the graphical representation of the player and updates the server to be free).
+
+        Arguments: NA
+
+        Output: NA
+        """
         self.color('grey')
         self.move(self.server.position[0] + 50 + random(), self.server.position[1] - 50 + random())
         self.server.players = self.server.players[1:]
@@ -66,10 +157,20 @@ class Player(Turtle):
         self.served = True
 
 class SelfishPlayer(Player):
+    """
+    A class for a player who acts selfishly (estimating the amount of time that they will wait and comparing to a value of service). The only modification is the arrive method that now allows players to balk.
+    """
     def __init__(self, lmbda, mu, queue, server, speed, costofbalking):
         Player.__init__(self, lmbda, mu, queue, server, speed)
         self.costofbalking = costofbalking
     def arrive(self, t):
+        """
+        As described above, this method allows players to balk if the expected time through service is larger than some alternative.
+
+        Arguments: t - time of arrival (a float)
+
+        Output: NA
+        """
         self.penup()
         self.arrivaldate = t
         self.color('red')
@@ -81,13 +182,30 @@ class SelfishPlayer(Player):
             self.balk()
             self.balked = True
     def balk(self):
+        """
+        Method to make player balk.
+
+        Arguments: NA
+
+        Outputs: NA
+        """
         self.move(random(), self.queue.position[1] - 25 + random())
 
 class OptimalPlayer(Player):
+    """
+    A class for a player who acts within a socially optimal framework (using the threshold from Naor's paper). The only modification is the arrive method that now allows players to balk and a new attribute for the Naor threshold.
+    """
     def __init__(self, lmbda, mu, queue, server, speed, naorthreshold):
         Player.__init__(self, lmbda, mu, queue, server, speed)
         self.naorthreshold = naorthreshold
     def arrive(self, t):
+        """
+        A method to make player arrive. If more than Naor threshold are present in queue then the player will balk.
+
+        Arguments: t - time of arrival (float)
+
+        Outputs: NA
+        """
         self.penup()
         self.arrivaldate = t
         self.color('gold')
@@ -99,6 +217,9 @@ class OptimalPlayer(Player):
             self.balk()
             self.balked = True
     def balk(self):
+        """
+        A method to make player balk.
+        """
         self.move(10 + random(), self.queue.position[1] - 25 + random())
 
 class Queue():
